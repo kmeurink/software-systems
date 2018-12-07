6bd7adb8a700231a5e6af64e2d416a22a4703ccc
kmeurink <kester.meurink@gmail.com>
2018-12-07 15:07:58 +0100

    Added better descriptions for the different methods.

--- week4/MergeSort.java
+++ week4/MergeSort.java
@@ -9,26 +9,35 @@ public class MergeSort {
     
     
     //Commands:
-	
+	/**
+	 * Start of the mergesort sorting algorithm.
+	 * @param list - the list that is to be sorted.
+	 */
     public static <E extends Comparable<E>>
-    void mergesort(List<E> list) {
+    	void mergesort(List<E> list) {
     	List<E> newList = split(list);
     	list.clear();
     	list.addAll(newList);
     }
 	
     /**
-     * Method to split the original list into lists of one element.
-     * @param list - The original list.
+     * Method to split the list supplied into lists of half the original length.
+     * @param list - The list that should be halved.
      */
     public static <E extends Comparable<E>> List<E> split(List<E> list) {
+    	//Initiate the list that will contain the ordered sequence.
     	List<E> resultList = new ArrayList<E>();
 
-    	//Check if the list contains less than two elements, at which point we continue.
+    	//Check if the list contains less than two elements, at which point it moves beyond split().
     	if (list.size() <= 1) {
     		resultList.addAll(list);
     		return resultList;
     	}
+    	/*
+    	 * Determine the index at which the list must be halved 
+    	 * and create a left and right half of the list.
+    	 * Then recursively call the split() method on both halves.
+      	 */
     	int mid = list.size() / 2;
     	List<E> leftList = list.subList(0, mid);
     	List<E> rightList = list.subList(mid, list.size());
@@ -41,10 +50,10 @@ public class MergeSort {
     	return resultList;
     }
     /**
-     * Method to merge the singular lists in an ordered sequence.
-     * @param list - The lists consisting of singular values.
-     * @param low - The lowest index of the list.
-     * @param high - The highest index of the list.
+     * Method to merge the lists into an ordered sequence.
+     * @param list - The lists consisting of half the values of the supplied list.
+     * @param leftList - The left half of the original list.
+     * @param rightList - The right half of the original list.
      */
     public static <E extends Comparable<E>> List<E> merge(List<E> list, List<E> leftList, List<E> rightList) {
     	List<E> resultList = new ArrayList<E>(leftList.size() + rightList.size());
