2b0f1cd0f84af887bcaeaf4ec0983b48aff27467
kmeurink <kester.meurink@gmail.com>
2018-12-10 10:48:25 +0100

    Finished P4.9

--- week4/DoublyLinkedList.java
+++ week4/DoublyLinkedList.java
@@ -19,12 +19,29 @@ public class DoublyLinkedList<Element> {
     //@ ensures this.getNode(index).equals(element);
     public void add(int index, Element element) {
         // TODO: implement, see exercise P-4.9
+    	Node newNode = new Node(element);
+    	//Reassigning the reference to the next node of the node in front of the one to be added.
+    	Node p = getNode(index - 1);
+    	newNode.next = p.next;
+    	p.next = newNode;
+    	//Reassigning the reference to the previous node of the node behind of the one to be added.
+    	Node b = getNode(index + 1);
+    	newNode.previous = b.previous;
+    	b.previous = newNode;
+    	size = size + 1;
     }
 
     //@ requires 0 <= index && index < this.size;
     //@ ensures this.size == \old(size) - 1;
     public void remove(int index) {
         // TODO: implement, see exercise P-4.9
+    	//Reassigning the reference to the next node of the node in front of the one to be removed.
+    	Node p = getNode(index - 1);
+    	p.next = p.next.next;
+    	//Reassigning the reference to the previous node of the node behind of the one to be removed.
+    	Node b = getNode(index + 1);
+    	b.previous = b.previous.previous;
+    	size = size - 1;
     }
 
     //@ requires 0 <= index && index < this.size;
