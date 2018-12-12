6fc536cc92ec95eff1e93aab952e869cb07d2559
kmeurink <kester.meurink@gmail.com>
2018-12-12 16:05:31 +0100

    Finished P5.7 continuing to P 5.8

--- week5/ComputerPlayer.java
+++ week5/ComputerPlayer.java
@@ -1,5 +1,32 @@
 package ss.week5;
 
-public class ComputerPlayer {
+import ss.week4.tictactoe.*;
+/**
+ * Class for maintaining a computer player in Tic Tac Toe. Module 2 lab assignment
+ */
 
+public class ComputerPlayer extends Player {
+	private Strategy strategy;
+	
+    public ComputerPlayer(Mark mark, Strategy strategy) {
+        super(strategy.getName() + "-" + mark.name(), mark);
+        this.strategy = strategy;
+    }
+    
+    public ComputerPlayer(Mark mark) {
+        super("NaiveStrategy-" + mark, mark);
+    }
+    
+    @Override
+	public int determineMove(Board board) {
+		return strategy.determineMove(board, this.getMark());
+	}
+ 
+	public Strategy inspect() {
+		return strategy;
+	}
+
+	public void update(Strategy strat) {
+		this.strategy = strat;
+	}
 }
