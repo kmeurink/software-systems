46d5c92263cbf9d94c7ad4278bdec6cd55b8eb21
kmeurink <kester.meurink@gmail.com>
2018-12-13 16:29:49 +0100

    Continuing to work on reccommended exercises week 5. Finished chapter 14

---
+++ additional/week5/InsertSort.java
@@ -0,0 +1,50 @@
+package ss.additional.week5;
+
+/**
+ * Class modelling the insertion sort algorithm
+ * @author kester.meurink
+ *
+ */
+
+public class InsertSort {
+	
+	private int[] array;
+	public InsertSort() {
+	}
+	
+	public void sort(int[] array) {
+		this.array = array;
+		for (int i=1; i<this.array.length; ++i) {
+			int key = this.array[i]; 
+            int j = i-1; 
+              /* Move elements of arr[0..i-1], that are 
+               greater than key, to one position ahead 
+               of their current position */
+            while (j >= 0 && this.array[j] > key) {
+            	this.array[j + 1] = this.array[j]; 
+                j = j - 1; 
+            } 
+            this.array[j + 1] = key; 
+        } 
+	}
+	
+    /* A utility function to print array of size n*/
+    static void printArray(int arr[]) { 
+        int n = arr.length; 
+        for (int i = 0; i < n; ++i) 
+            System.out.print(arr[i] + " "); 
+  
+        System.out.println(); 
+    } 
+  
+    // Driver method 
+    public static void main(String args[]) 
+    {         
+        int arr[] = {12, 11, 13, 5, 6}; 
+  
+        InsertSort ob = new InsertSort();         
+        ob.sort(arr); 
+          
+        printArray(arr); 
+    } 
+}
---
+++ additional/week5/Sorting.java
@@ -0,0 +1,48 @@
+package ss.additional.week5;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+//Exercise 14.8
+public class Sorting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+	/**
+	 * Method that merges the specified lists
+	 * @param list1
+	 * @param list2
+	 * @param order
+	 * @return
+	 */
+	// @require list1 and list2 are sorted
+	// @ensure merge(list1, list2) is sorted
+	public static <E> List<E> merge (List<E> list1, List<E> list2, Comparator<E> order) {
+		int e1 = 0;
+		int e2 = 0;
+		List<E> merged = new ArrayList<E>();
+		while (e1 < list1.size() && e2 < list2.size()) {
+			if (order.compare(list1.get(e1), list2.get(e2)) <= 0) {
+				merged.add(list1.get(e1));
+				e1 = e1 + 1;
+			} else {
+				merged.add(list2.get(e2));
+				e2 = e2 + 1;
+			}
+		}
+		while (e1 < list1.size()) {
+			merged.add(list1.get(e1));
+			e1 = e1 + 1;
+		}
+		while (e2 < list2.size()) {
+			merged.add(list2.get(e2));
+			e2 = e2 + 1;
+		}
+		return merged;
+	}
+
+}
