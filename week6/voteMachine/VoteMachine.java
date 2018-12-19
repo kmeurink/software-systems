abf02c5cad0d4c7696758054ac27bea0c3d3fe94
kmeurink <kester.meurink@gmail.com>
2018-12-19 14:44:41 +0100

    Working on P6.11

---
+++ week6/voteMachine/VoteMachine.java
@@ -0,0 +1,32 @@
+package ss.week6.voteMachine;
+
+/**
+ * Controller class for the voting machine.
+ * @author kester.meurink
+ *
+ */
+public class VoteMachine {
+	
+	//Main
+	public static void main(String[] args) {
+		new VoteMachine();
+	}
+	
+	//Constructor:
+	public VoteMachine() {
+		
+	}
+	
+	//Queries:
+	
+	
+	//Commands:
+	
+	/**
+	 * Method to start the machine.
+	 */
+	public void start() {
+		
+	}
+
+}
