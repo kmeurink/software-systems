8c410d35d9383856096d3db6a18d7214e6fb4ead
kmeurink <kester.meurink@gmail.com>
2019-01-08 15:34:35 +0100

    Changed TestSyncConsole to use with P7.15

--- week7/threads/TestSyncConsole.java
+++ week7/threads/TestSyncConsole.java
@@ -1,6 +1,9 @@
 package ss.week7.threads;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TestSyncConsole extends Thread {
+	ReentrantLock lock = new ReentrantLock();
 	public static void main(String[] args) {
 		new TestSyncConsole("Thread A").start();
 		new TestSyncConsole("Thread B").start();
@@ -9,6 +12,11 @@ public class TestSyncConsole extends Thread {
 	public TestSyncConsole(String name) {
 		this.setName(name);
 	}
+	
+	public TestSyncConsole(String name, ReentrantLock lock) {
+		this.setName(name);
+		this.lock = lock;
+	}
 
 	// overrides Thread.run
 
@@ -16,10 +24,12 @@ public class TestSyncConsole extends Thread {
 		sum();
 	}
 
-	private synchronized void sum() {
+	private void sum() {
+		lock.lock();
 		int e1 = SyncConsole.readInt(currentThread().getName() + " Get number 1: ");
 		int e2 = SyncConsole.readInt(currentThread().getName() + " Get number 2: ");
 		int sum = e1 + e2;
 		SyncConsole.println(this.getName() + " " + e1 + " + " + e2 + " = " + sum);
+		lock.unlock();
 	}
 }
