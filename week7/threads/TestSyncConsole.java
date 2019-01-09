ef5b675b9843bed47a010724f915ff5164b666c2
kmeurink <kester.meurink@gmail.com>
2019-01-09 10:06:25 +0100

    Added static to lock, to ensure everything happens in order

--- week7/threads/TestSyncConsole.java
+++ week7/threads/TestSyncConsole.java
@@ -3,7 +3,7 @@ package ss.week7.threads;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class TestSyncConsole extends Thread {
-	ReentrantLock lock = new ReentrantLock();
+	static ReentrantLock lock = new ReentrantLock();
 	public static void main(String[] args) {
 		new TestSyncConsole("Thread A").start();
 		new TestSyncConsole("Thread B").start();
